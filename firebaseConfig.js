40985513a664250a3de0b10aa935a3261f3d164a
Heng Hong <118714008+pv314@users.noreply.github.com>
2023-10-29 00:59:32 -0400

    Add files via upload
    Modified the config file a little to hide sensitive Firebase values, as we have set up these keys in the local .env file.

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -5,13 +5,22 @@ import { getFirestore } from "firebase/firestore";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: 'AIzaSyAkBSIL_H51f-3vzsUJmSfqnknNHPFkX8c',
+//   authDomain: 'primal-ivy-319500.firebaseapp.com',
+//   projectId: 'primal-ivy-319500',
+//   storageBucket: 'primal-ivy-319500.appspot.com',
+//   messagingSenderId: '628446212322',
+//   appId: '1:628446212322:web:ee1a2d05c0aba84033eebb',
+// };
+
 const firebaseConfig = {
-  apiKey: 'AIzaSyAkBSIL_H51f-3vzsUJmSfqnknNHPFkX8c',
-  authDomain: 'primal-ivy-319500.firebaseapp.com',
-  projectId: 'primal-ivy-319500',
-  storageBucket: 'primal-ivy-319500.appspot.com',
-  messagingSenderId: '628446212322',
-  appId: '1:628446212322:web:ee1a2d05c0aba84033eebb',
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
